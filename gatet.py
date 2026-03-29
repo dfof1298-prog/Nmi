ba0d37c563e16e6f7de7dc0c70326f7c8568738e
dfof1298-prog <dfof1298@gmail.com>
2026-03-30 00:06:11 +0200

    Update donation URL in chkk function

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://donbosco2000.org/donations/givewp-donation-form/?lang=en"
+	urll="https://www.theflorentine.net/support-the-florentine/"
 	price="0.50"
 	res=requests.get(f'https://pqy2-production.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
